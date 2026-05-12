e9fc21774c273fe63d40f3c7fbfed83267643ff8
Lowaga Rajapaksha <lowaga2003@gmail.com>
2026-05-12 15:14:37 +0530

    Initial commit: Spring Boot project foundation and pom.xml update

--- src/main/java/com/rental/vehiclerentalsystem_v2/VehicleRentalSystemV2Application.java
+++ src/main/java/com/rental/vehiclerentalsystem_v2/VehicleRentalSystemV2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VehicleRentalSystemV2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehicleRentalSystemV2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentalSystemV2Application.class, args);
+    }
 
 }
