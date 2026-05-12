d5a941660ae9d8c0758503241580d1579025f281
Lowaga Rajapaksha <lowaga2003@gmail.com>
2026-05-12 15:02:37 +0530

    Initial commit: Spring Boot project foundation

---
+++ src/main/java/com/rental/vehiclerentalsystem_v2/VehicleRentalSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.rental.vehiclerentalsystem_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalSystemV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalSystemV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rental/vehiclerentalsystem_v2/VehicleRentalSystemV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.vehiclerentalsystem_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalSystemV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
