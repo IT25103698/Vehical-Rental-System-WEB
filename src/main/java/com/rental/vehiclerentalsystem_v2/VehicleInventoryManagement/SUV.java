aa112600eb6b1d766c80515ac1037a9748590e48
De Silva G.K.M. <IT25102949@my.sliit.lk>
2026-05-14 19:35:54 +0530

    SUV child class created in inventoryManagement package

---
+++ src/main/java/com/rental/vehiclerentalsystem_v2/VehicleInventoryManagement/SUV.java
@@ -0,0 +1,18 @@
+package com.rental.vehiclerentalsystem_v2.VehicleInventoryManagement;
+
+public class SUV extends Vehicle {
+    private boolean isAWD;
+
+    public SUV(String vehicleId, String brand, String model, String licensePlate, double dailyRate, boolean isAWD) {
+        super(vehicleId, brand, model, licensePlate, dailyRate);
+        this.isAWD = isAWD;
+    }
+
+    public boolean isAWD() { return isAWD; }
+
+    @Override
+    public String toFileString() {
+        return super.toFileString() + ",SUV," + isAWD;
+    }
+}
+
