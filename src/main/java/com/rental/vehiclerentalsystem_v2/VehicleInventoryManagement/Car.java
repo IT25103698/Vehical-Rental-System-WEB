167d302a738687bd63481360a154ac6a7b5a9f61
De Silva G.K.M. <IT25102949@my.sliit.lk>
2026-05-14 23:51:57 +0530

    Car Child Class created in Vehicle  inventoryManagement package

---
+++ src/main/java/com/rental/vehiclerentalsystem_v2/VehicleInventoryManagement/Car.java
@@ -0,0 +1,18 @@
+package com.rental.vehiclerentalsystem_v2.VehicleInventoryManagement;
+
+public class Car extends Vehicle {
+    private String fuelType; // e.g., Petrol, Diesel, Electric
+
+    public Car(String vehicleId, String brand, String model, String licensePlate, double dailyRate, String fuelType) {
+        super(vehicleId, brand, model, licensePlate, dailyRate); // Send common data to Parent
+        this.fuelType = fuelType;
+    }
+
+    public String getFuelType() { return fuelType; }
+
+    @Override
+    public String toFileString() {
+        return super.toFileString() + ",Car," + fuelType;
+    }
+}
+
